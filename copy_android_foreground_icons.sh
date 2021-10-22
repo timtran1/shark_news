2945a13d56b17f502c16f240ab87acfb0dccf77f
trung@kiuglobal.com <trung@kiuglobal.com>
2021-10-22 15:02:48 +0700

    update

---
+++ copy_android_foreground_icons.sh
@@ -0,0 +1,6 @@
+cp ./resources/android/icon/drawable-hdpi-icon.png ./android/app/src/main/res/mipmap-hdpi/ic_launcher_foreground.png
+cp ./resources/android/icon/drawable-mdpi-icon.png ./android/app/src/main/res/mipmap-mdpi/ic_launcher_foreground.png
+cp ./resources/android/icon/drawable-xhdpi-icon.png ./android/app/src/main/res/mipmap-xhdpi/ic_launcher_foreground.png
+cp ./resources/android/icon/drawable-xxhdpi-icon.png ./android/app/src/main/res/mipmap-xxhdpi/ic_launcher_foreground.png
+cp ./resources/android/icon/drawable-xxxhdpi-icon.png ./android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_foreground.png
+echo "Copy fix applied."
